a5309985c0745811e01d371ef07e457b3ed35f11
Brian Mock <mock.brian@gmail.com>
2011-12-05 22:29:30 -0800

    Revert "Added sorting in the GUI..."
    This reverts commit e06f554f84cca6a58fe108aee9d41b144a6339c6.

--- Grid.java
+++ Grid.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
-import java.util.List;
 
 public class Grid extends JPanel {
     private final Color bgColor = new Color(0x222222);
@@ -55,18 +54,7 @@ public class Grid extends JPanel {
     private void populateGrid() {
         clearGrid();
 
-        World world;
-        NpcPopulation pop;
-        Collection<NpcIndividual> inds;
-        List<NpcIndividual> linds;
-        Iterator<NpcIndividual> it;
-
-        world = gui.getWorld();
-        pop   = (NpcPopulation) world.getPopulation();
-        inds  = pop.getIndividuals();
-        linds = new ArrayList(inds);
-        Collections.sort(linds);
-        it    = linds.iterator();
+        Iterator<NpcIndividual> it = ((NpcPopulation) gui.getWorld().getPopulation()).getIndividuals().iterator();
         for (int r=0; r < rows; r++) {
             for (int c=0; c < cols; c++) {
                 if (it.hasNext()) {
