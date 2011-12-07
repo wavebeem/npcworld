7840c3e0842a47daad58464a7676a833e2d2ef9a
Brian Mock <mock.brian@gmail.com>
2011-12-06 19:02:22 -0800

    NullIcon paints a background now.

--- NullIcon.java
+++ NullIcon.java
@@ -1,6 +1,14 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class NullIcon extends JComponent {
-    public void paintComponent() {
+    private Color bgColor = new Color(0x111111);
+
+    public void paintComponent(Graphics g) {
+        final int w = getWidth();
+        final int h = getHeight();
+
+        g.setColor(bgColor);
+        g.fillRect(0, 0, w, h);
     }
 }
