9292b00aaa442544d605145bdbd8adca284d8d82
Ashley Fisher <fish.ash@gmail.com>
2011-12-09 15:01:05 -0800

    NpcWorld uses Settings better, deleted merge

--- Const.java
+++ Const.java
@@ -2,8 +2,8 @@ public class Const {
     public static final int MALE   = 0;
     public static final int FEMALE = 1;
 
+    public static final int PLAYING  = 0;
     public static final int EATING   = 1;
     public static final int SLEEPING = 2;
     public static final int MATING   = 3;
-    public static final int PLAYING  = 4;
 }
