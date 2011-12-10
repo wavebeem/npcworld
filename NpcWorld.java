ff2241825467f33b0c908cb19d0823165f2a7262
Ashley Fisher <fish.ash@gmail.com>
2011-12-09 18:19:17 -0800

    Started implementing migrating and made some fixes

--- NpcWorld.java
+++ NpcWorld.java
@@ -10,10 +10,11 @@ public class NpcWorld implements World {
     private ArrayList<NpcIndividual> matingPoolMales;
     private ArrayList<NpcIndividual> matingPoolFemales;
 
+    private ArrayList<NpcIndividual> migrationPool;
+
     // current availability
     private int eatingAvailability;
     private int sleepingAvailability;
-    private int matingAvailability;
 
     private int stepNumber;
 
@@ -37,7 +38,6 @@ public class NpcWorld implements World {
 
         eatingAvailability   = Settings.eatingCapacity;
         sleepingAvailability = Settings.sleepingCapacity;
-        matingAvailability   = Settings.matingCapacity;
     }
 
     public NpcPopulation getPopulation() {
@@ -112,7 +112,6 @@ public class NpcWorld implements World {
     private void freeAvailabilities() {
         eatingAvailability   = Settings.eatingCapacity;
         sleepingAvailability = Settings.sleepingCapacity;
-        matingAvailability   = Settings.matingCapacity;
 
         // go through all of the individuals and decrement availabilities
         // if the individual is not done with its action
@@ -122,8 +121,6 @@ public class NpcWorld implements World {
                     eatingAvailability--;
                 } else if (i.getCurrentAction() == Const.SLEEPING) {
                     sleepingAvailability--;
-                } else if (i.getCurrentAction() == Const.MATING) {
-                    matingAvailability--;
                 }
             }
         }
@@ -137,20 +134,27 @@ public class NpcWorld implements World {
 
             if (eatingAvailability   > 0) actions.add(Const.EATING  );
             if (sleepingAvailability > 0) actions.add(Const.SLEEPING);
-            if (matingAvailability   > 0) actions.add(Const.MATING  );
+            if (Settings.migrationEnabled) actions.add(Const.MIGRATING);
+            actions.add(Const.MATING);
+            actions.add(Const.PLAYING);
 
             int action = ind.chooseAction(actions);
 
-            if      (action == Const.EATING)   eatingAvailability--;
-            else if (action == Const.SLEEPING) sleepingAvailability--;
+            if      (action == Const.EATING)    eatingAvailability--;
+            else if (action == Const.SLEEPING)  sleepingAvailability--;
             else if (action == Const.MATING) {
-                matingAvailability--;
-                if (((NpcDna)ind.getDna()).getGender() == Const.MALE) {
+                if (ind.getGender() == Const.MALE) {
                     matingPoolMales.add(ind);
                 } else {
                     matingPoolFemales.add(ind);
                 }
             }
+            else if (action == Const.MIGRATING) {
+                migrationPool.add(ind);
+                population.remove(ind);
+                availableIDs[ind.getID()] = true;
+            }
+
         }
 
         ind.decreaseStepsRemaining();
@@ -215,5 +219,17 @@ public class NpcWorld implements World {
         double curDeathChance = Settings.deathChance + Settings.deathChanceChange * ageDiff;
 
         return Math.min(curDeathChance, Settings.deathChanceMax);
+    }
+
+    public int percentMale() {
+        int numMales = 0;
+        for (NpcIndividual ind : population.getIndividuals()) {
+            if (ind.getGender() == Const.MALE) numMales++;
+        }
+        return (int)((100.0 * numMales) / population.getSize());
+    }
+
+    public ArrayList<NpcIndividual> getMigrationPool() {
+        return migrationPool;
     }
 }
