c6e45f7b56c68c76291a831485788733058abe83
Trevor Griswold <tgriswol@willamette.edu>
2011-12-09 19:12:13 -0800

    Added step() and getAdjacentWorlds(int, int)

--- NpcUniverse.java
+++ NpcUniverse.java
@@ -15,14 +15,25 @@ public class NpcUniverse implements Universe {
     }
 
     public void step(){
-
+        for(int row = 0; row < Const.UNIVERSE_ROWS; row++){
+            for(int col = 0; col < Const.UNIVERSE_COLS; col++){
+                worlds[row][col].step();
+            }
+        }
     }
 
     private void migrateWorlds(){
-
+        
     }
 
-    private ArrayList<World> getAdjacentWorlds(int row, int col){
-        return null;
+    private ArrayList<NpcWorld> getAdjacentWorlds(int row, int col){
+        ArrayList<NpcWorld> adjacentWorlds = new ArrayList<NpcWorld>();
+        if(row >= 0 && row < Const.UNIVERSE_ROWS && col >= 0 && col < Const.UNIVERSE_COLS){
+            if(row < Const.UNIVERSE_ROWS-1) adjacentWorlds.add(worlds[row+1][col]); 
+            if(row > 0)      adjacentWorlds.add(worlds[row-1][col]); 
+            if(col < Const.UNIVERSE_COLS-1) adjacentWorlds.add(worlds[row][col+1]);
+            if(col > 0)      adjacentWorlds.add(worlds[row][col-1]);
+        }
+        return adjacentWorlds;
     }
 }
